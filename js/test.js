ba2859226883a02c536f9e9b8f6c19bd3e0802cd
Ya-ing Liu <lyj5838309@hotmail.com>
2017-08-05 15:52:54 +0800

    background

---
+++ js/test.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+var zigzag = new mojs.Shape({
+	shape: 'zigzag',
+	points: 11,
+	radius: 25,
+	radiusY: 20,
+	left: '25%',
+	fill: 'none',
+	stroke: 'deeppink',
+	isShowStart: true,
+});
+
+var curve = new mojs.Shape({
+	shape: 'curve',
+	points: 11,
+	radius: 25,
+	radiusY: 20,
+	left: '50%',
+	fill: 'none',
+	stroke: 'deeppink',
+	isShowStart: true,
+});
+
+var cross = new mojs.Shape({
+	shape: 'cross',
+	points: 11,
+	radius: 25,
+	radiusY: 20,
+	left: '75%',
+	fill: 'none',
+	stroke: 'deeppink',
+	isShowStart: true,
+	y: -25,
+});
