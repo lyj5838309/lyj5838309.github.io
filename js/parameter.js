21e5e26e9ea0f836cd7826d281e5fd8ae88796fe
Ya-ing Liu <lyj5838309@hotmail.com>
2017-08-23 16:46:12 +0800

    static

--- js/parameter.js
+++ js/parameter.js
@@ -18,6 +18,10 @@ var invitee_str = urlParams.title + urlParams.name + urlParams.subtitle + urlPar
 var invitee_name_str = urlParams.name + urlParams.subtitle + urlParams.other;
 var inviter_str = urlParams.inviter;
 
-
-$(".invitee").text(invitee_str);
-$(".inviter").text(inviter_str);
+if (isNaN(invitee_str)) {
+	$(".taitou").text("");
+	$(".luokuan").text("戴望州 刘雅静 敬邀");
+} else {
+	$(".invitee").text(invitee_str);
+	$(".inviter").text(inviter_str);
+}
