d3f3fb4780b2a2d11da2a74f40896b819e344774
Ya-ing Liu <lyj5838309@hotmail.com>
2017-08-24 01:51:27 +0800

    my send mail

--- js/reg.js
+++ js/reg.js
@@ -13,7 +13,7 @@ function dec_reg_num() {
 	$(".num_reg").text(num_reg);
 }
 
-function send_mail() {
+function my_send_mail() {
 	var msg = "邀请人：" + inviter_str + "\n";
 	msg += num_reg.toString;
 	Email.send("hal9k1@126.com",
@@ -27,4 +27,4 @@ function send_mail() {
 $(".reg_dec").on('click', dec_reg_num);
 $(".reg_add").on('click', add_reg_num);
 
-$(".image").on('click', send_mail);
+$(".image").on('click', my_send_mail);
